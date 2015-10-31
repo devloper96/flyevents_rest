220302b9cdeeecc1c3d15865293b229685101519
smit thakkar <smit@smits-MBP.mshome.net>
2015-10-31 20:22:36 +0530

    commit

--- app.py
+++ app.py
@@ -24,6 +24,14 @@ def sendsms():
 	print("sms")
  	return code
 
+@app.route('/getImage')
+def getImage():
+	tu="www.google.com"
+	url = "http://api.page2images.com/restfullink?p2i_url=www.google.com&p2i_key=6658882a4001ef71&p2i_screen=1024x768&p2i_device=6&p2i_size=512x384"
+	data = requests.get(url)
+	print(data.content)
+	return data.content
+
 def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
 	return ''.join(random.choice(chars) for _ in range(size))	
 if __name__ == '__main__':
